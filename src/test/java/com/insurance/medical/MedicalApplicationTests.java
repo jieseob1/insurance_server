0559883577b89b9dbcef2dea92805aa340abb15e
jiseob park <jieseob1@github.com>
2024-06-26 23:56:00 +0900

    init insurance server

---
+++ src/test/java/com/insurance/medical/MedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
